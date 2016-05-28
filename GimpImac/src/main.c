065381e1c4217d1cb49e88c0d145c79079178d3a
Amassan <antonin.leuret2@gmail.com>
2016-05-28 07:35:30 +0200

    Interface graphique

--- GimpImac/src/main.c
+++ GimpImac/src/main.c
@@ -12,6 +12,10 @@
 #include "outils.h"
 
 #include "Calque.h"
+
+float tabX[] = { 0.07 , 0.24 , 0.37 , 0.625 , 0.875 , 0.75 , 0.65 , 0.65 , 0.65 , 0.65 , 0.65 , 0.85 , 0.85 , 0.85 , 0.85 , 0.85 };
+float tabY[] = { 0.92 , 0.92 , 0.92 , 0.115 , 0.115 , 0.115 , 0.915 , 0.755 , 0.595 , 0.435 , 0.275 , 0.915 , 0.755 , 0.595 , 0.435 , 0.275 };
+	
 /// ///////////////////////////////////////////////////////////////////////////
 /// Tableau representant une image...
 unsigned char* image_base = NULL;
@@ -88,27 +92,60 @@ void clickMouse(int button,int state,int x,int y) {
     else { // state == GLUT_UP
         printf("relache\n");
     }
+    
+   
+		
+    if (x == 700) {
+		printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+	}
+    
     printf("Coordonnees du point clique %d %d\n",x,y);
 }
 
 /// ///////////////////////////////////////////////////////////////////////////
 /// fonction de personnalisation du rendu
 void mondessin() {
-    writeString(0,0,"Ecriture ecran");
-    fixeCouleur(1.0,0.0,0.0);
-    drawLigne(0.0,0.0,1.0,1.0);
-    fixeCouleur(1.0,1.0,0.0);
-    drawCarre(0.5,0.5,0.7,0.7);
-    fixeCouleur(1.0,0.0,0.0);
-    drawCarreVide(0.85,0.2,0.95,0.25);
-    writeString(0.86/**800+1*/,0.21/**600+1*/,"TEST");
-    drawDisque(0.1,0.5,0.1);
-    drawDisque(0.3,0.5,0.1);
-    drawDisque(0.5,0.5,0.1);
-    drawDisque(0.7,0.5,0.1);
-    drawDisque(0.9,0.5,0.1);
-    fixeCouleur(0.0,0.0,1.0);
-    drawCercle(0.7,0.5,0.02);
+   // writeString(0,0,"Section image");
+   // fixeCouleur(1.0,0.0,0.0);
+   // drawLigne(0.0,0.0,1.0,1.0);
+   // fixeCouleur(1.0,1.0,0.0);
+   // drawCarre(0.5,0.5,0.7,0.7);
+    fixeCouleur(0.96,0.65,0.16);
+    drawCarre(0.02,0.96,0.15,0.88);  	/// import export histogramme
+    drawCarre(0.17,0.96,0.30,0.88);
+    drawCarre(0.32,0.96,0.45,0.88);
+    fixeCouleur(0.9,0.4,0.3);
+    drawCarre(0.60,0.15,0.65,0.08); 	/// choix du calque
+    drawCarre(0.85,0.15,0.90,0.08); 
+    drawCarre(0.68,0.15,0.82,0.08);  
+    fixeCouleur(1.0,1.0,1.0);
+    writeString(0.71,0.915,"LUMINOSITE");
+    writeString(0.71,0.755,"CONTRASTE");
+    writeString(0.71,0.595,"OPACITE");
+    writeString(0.71,0.435,"INVERSER");
+    writeString(0.71,0.275,"SEPIA");
+    writeString(0.03,0.915,"Importer image");
+    writeString(0.18,0.915,"Exporter image");
+    writeString(0.34,0.915,"Histogramme");
+    writeString(0.615,0.11,"<<");
+    writeString(0.87,0.11,">>");
+    writeString(0.7,0.11,"Suppr Calque");
+    
+    fixeCouleur(0.3,0.64,0.87);
+    drawDisque(0.65,0.915,0.02);	/// luminosite
+    drawDisque(0.65,0.755,0.02);	/// contraste
+    drawDisque(0.65,0.595,0.02);	/// opacite
+    drawDisque(0.65,0.435,0.02);	/// inverser
+    drawDisque(0.65,0.275,0.02);	/// sepia
+    
+    fixeCouleur(0.25,0.83,0.49);
+    drawDisque(0.85,0.915,0.02);	/// luminosite
+    drawDisque(0.85,0.755,0.02);	/// contraste
+    drawDisque(0.85,0.595,0.02);	/// opacite  
+    drawDisque(0.85,0.435,0.02);	/// inverser  
+    drawDisque(0.85,0.275,0.02);	/// sepia
+    //fixeCouleur(0.0,0.0,1.0);
+    //drawCercle(0.7,0.5,0.02);
     fixeCouleur(1.0,1.0,1.0);
 }
 
@@ -193,7 +230,7 @@ int main(int argc, char** argv) {
 
     TypeOperation op = normal;
     Calque calque;
-    initCalque(&calque, "/home/quentin/Bureau/ProjetC/GimpImac/images/F16.512.ppm", op, 1);
+    initCalque(&calque, "../images/F16.512.ppm", op, 1);
     printf("%d  %d\n",calque.longueur,calque.hauteur);
 
 
